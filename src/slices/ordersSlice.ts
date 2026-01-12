bca9acc0dddb488a499599ac6c7e01556c822634
bair <badmaiev2002@mail.ru>
2026-01-12 16:53:23 +0300

    order fix

--- src/slices/ordersSlice.ts
+++ src/slices/ordersSlice.ts
@@ -1,20 +1,33 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { TOrder } from '@utils-types';
-import { getOrdersApi } from '../utils/burger-api';
+import { getOrderByNumberApi, getOrdersApi } from '../utils/burger-api';
 
 type TInitialState = {
   userOrders: TOrder[] | null;
+  orderByNumber: TOrder | null;
   loading: boolean;
 };
 
 export const initialState: TInitialState = {
   userOrders: null,
+  orderByNumber: null,
   loading: false
 };
 
 export const fetchUserOrders = createAsyncThunk(
   'orders/getUserOrders',
   async () => getOrdersApi()
+);
+
+export const getOrderByNumber = createAsyncThunk(
+  'orders/getOrderByNumber',
+  async (number: number) => {
+    const response = await getOrderByNumberApi(number);
+    if (response.success && response.orders && response.orders.length > 0) {
+      return response.orders[0];
+    }
+    return Promise.reject(new Error('Order not found'));
+  }
 );
 
 const ordersSlice = createSlice({
@@ -27,6 +40,7 @@ const ordersSlice = createSlice({
   },
   selectors: {
     selectUserOrders: (state) => state.userOrders,
+    selectOrderByNumber: (state) => state.orderByNumber,
     selectOrdersLoading: (state) => state.loading
   },
   extraReducers: (builder) => {
@@ -40,6 +54,17 @@ const ordersSlice = createSlice({
       .addCase(fetchUserOrders.fulfilled, (state, action) => {
         state.loading = false;
         state.userOrders = action.payload;
+      })
+      .addCase(getOrderByNumber.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getOrderByNumber.rejected, (state) => {
+        state.loading = false;
+        state.orderByNumber = null;
+      })
+      .addCase(getOrderByNumber.fulfilled, (state, action) => {
+        state.loading = false;
+        state.orderByNumber = action.payload;
       });
   }
 });
@@ -49,6 +74,9 @@ export const { removeUserOrders } = ordersSlice.actions;
 export const selectUserOrders = (
   state: import('../services/store').RootState
 ) => state.orders.userOrders;
+export const selectOrderByNumber = (
+  state: import('../services/store').RootState
+) => state.orders.orderByNumber;
 export const selectOrdersLoading = (
   state: import('../services/store').RootState
 ) => state.orders.loading;
